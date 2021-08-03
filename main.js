4c42744ca63d8c2803eed40d1fbd9392445aeb7b
Dario Perez <darioperez1415@gmail.com>
2021-08-02 23:30:27 -0500

    changed form

--- main.js
+++ main.js
@@ -9,23 +9,20 @@ const renderToDom = (divId,textToRender) => {
 
 const addsForm = () => {
     const form =`
-    <form class="form-inline">
-  <div class="form-group mb-2">
-    <label for="staticEmail2" class="sr-only">Student</label>
-    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com">
+    <div class="input-group">
+    <p> Student's Name </p>
+    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Name"
+      aria-describedby="search-addon" />
+    <button type="button" class="btn btn-outline-primary">Sort!</button>
   </div>
-  <div class="form-group mx-sm-3 mb-2">
-    <label for="inputPassword2" class="sr-only">Password</label>
-    <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-primary mb-2">Sort!</button>
-</form>
     `
     renderToDom("#formContainer", form);
 }
 
-//Frist button function
-const sort 
+//Frist button function - Will need this 
+//const sortButton = () => {
+   // const sort = document.querySelector(#buttonContainer);
+}
 
 //This starts my application 
 const init = () => {
