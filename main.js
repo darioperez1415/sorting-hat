dff4b86b4fc3c4038487389da87b26f8342972d7
Dario Perez <darioperez1415@gmail.com>
2021-08-02 23:10:39 -0500

    Added some functions

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
-cosnt firstYears = [];
-cosnt expeledStudents = [];
+const firstYearContainer = [];
+const expeledStudents = [];
 
 // Render to Dom
 const renderToDom = (divId,textToRender) => {
     const selectedDiv = document.querySelector(divId);
     selectedDiv.innerHTML = textToRender;
 }
-//Creates event listners on Dom
 
-cosnt buttons = () => {
-
-    cosnt domString =`
-    <button type="button" class="btn btn-primary" id="sorting">Let's Start Sorting</button>
+const addsForm = () => {
+    const form =`
+    <form class="form-inline">
+  <div class="form-group mb-2">
+    <label for="staticEmail2" class="sr-only">Student</label>
+    <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com">
+  </div>
+  <div class="form-group mx-sm-3 mb-2">
+    <label for="inputPassword2" class="sr-only">Password</label>
+    <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-primary mb-2">Sort!</button>
+</form>
     `
-    renderToDom("#formContainer", domString);
+    renderToDom("#formContainer", form);
 }
 
+//Frist button function
+const sort 
+
 //This starts my application 
-// cosnt init = () => {
-//     buttons();
-// }
+const init = () => {
+    addsForm();
+}
+
+init();
