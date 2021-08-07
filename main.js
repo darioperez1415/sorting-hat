88691d629e9dcec55c04c2b010b95ff8a58861b8
Dario Perez <darioperez1415@gmail.com>
2021-08-07 11:24:58 -0500

    Added house color functions

--- main.js
+++ main.js
@@ -25,19 +25,19 @@ const welcomeCard = () => {
 //Frist event lister, after user clicks "letsBegin", this function adds form
 
 const firstEventListner = () => {
-  document.querySelector("#letsBegin").addEventListener("click",addsForm);
+  document.querySelector("#letsBegin").addEventListener("click", addsForm);
 }
 // Function for calling form for new students
 const addsForm = () => {
-  const newStudentForm = `
+  const domString = `
     <div id="form-entry" class="input-group">
     <label for"Name" class="form-label">Name</label> 
     <input type="text" class="form-control rounded" placeholder="Name" aria-label="Name"
       aria-describedby="search-addon" id="Name"/>
     <button type="button" id="sortButton" class="btn btn-outline-primary">Sort!</button>
   </div>`
-  renderToDom("#formContainer", newStudentForm);
-  document.querySelector("#sortButton").addEventListener("click",handleButtonClick);
+  renderToDom("#formContainer", domString);
+  document.querySelector("#sortButton").addEventListener("click", handleButtonClick);
 }
 //Function for house assingnment 
 const houseAssignmet = () => {
@@ -55,40 +55,87 @@ const houseAssignmet = () => {
 //Handles sort button click
 
 const handleButtonClick = (event) => {
-  event.preventDefault();
+  event.preventDefault(); //Prevents page refresh
   const sortClick = event.target.id;
   if (sortClick === "sortButton") {
 
-    const newSudent = {
+    const student = {
       name: document.querySelector("#Name").value,
       house: houseAssignmet(1, 5)
     };
-    firstYearContainer.push(newSudent);
-  studentAssignment(firstYearContainer)
+    firstYearContainer.push(student);
+    studentAssignment(firstYearContainer)
   }
 }
 
+// Takes house and assigns a color 
+
+const houseColor = (houseAssignmet) => {
+
+  if (houseAssignmet === "Griffandor") {
+  return '#AE0001';
+} else if (houseAssignmet = "Ravenclaw"){
+  return '#F0FFFF';
+}
+  else if (houseAssignmet = "HufflePuff") {
+    return '#ffcc00';
+} else {
+  return '#006600';
+}
+}
 //Passes cards into DOM
-const studentAssignment = (newSudent) =>{
-  let  newPerson= "";
-  newSudent.forEach((student) => {
-    newPerson += `
+const studentAssignment = (array) => {
+  let domString = "";
+  array.forEach((student) => {
+    domString += `
     <div class="card" style="width: 18rem;">
   <div class="card-body">
   <h5 class="card-title">"${student.name}"</h5>
     <p class="card-text">"${student.house}".</p>
-    <button type="button" id="sortButton" class="btn btn-outline-primary">Sort!</button>
+    <button type="button" id="Expelled" class="btn btn-outline-primary">Expel</button>
 </div>
     `;
   })
-  renderToDom("#firstYearContainer",newPerson);
+  renderToDom("#firstYearContainer", domString);
 };
 
+
+const expelButton = (event) => {
+
+  const expelClick = event.traget.id;
+
+  if (targetId === "Expelled") {
+    const darkSide = student.splice(expelClick, 1);
+    expeledStudents.push(darkSide[0]);
+    valdArray(expeledStudents);
+  }
+};
+
+//Creats array for Vald Cards
+const valdArray = (array) => {
+  let domString = ""
+  array.forEach((student) => {
+    domString += `    <div class="expelCard" style="width: 18rem;">
+    <div class="expelCardBody">
+    <h5 class="expelCardTitle">${student.name} </h5>
+    <p class="expelCardText">Has joined Voldemort's Army!</p>
+    </div>
+</div>
+`
+  });
+  renderToDom("#expeledStudents", domString);
+};
+
+
+// Handle botton clicks
+const buttonEvents = () => {
+  document.querySelector("#expeled").addEventListener("click", expelButton);
+}
 //This starts my application 
 const init = () => {
   welcomeCard();
   firstEventListner();
-
+  buttonEvents();
 }
 
 init();
